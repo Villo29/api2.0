8e2182917f325371e4190a1dc1c15d736a7efd25
David Ruiz <jesuruga@gmail.com>
2024-07-26 00:17:45 -0600

    [Fix] correcion

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -1,9 +1,9 @@
-import express, { Application } from 'express';
-import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
-import { MongoStudentRepository } from './repositories/MongoStudentRepository';
-import { StudentController } from './controllers/StudentController';
-import cors from 'cors';
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import express, { Application } from "express";
+import { MongoStudentRepository } from "./repositories/MongoStudentRepository";
+import { StudentController } from "./controllers/StudentController";
 
 dotenv.config();
 
@@ -12,35 +12,35 @@ const port = process.env.PORT || 2929;
 const mongoUri = process.env.MONGO_URI;
 const dbName = process.env.DB_NAME;
 
-// Configurar CORS para permitir todos los orígenes
-app.use(cors());
-
-// Configurar CORS para permitir un origen específico
-app.use(cors({ origin: 'http://localhost:5173' }));
-
-// Configurar para parsear JSON
-app.use(express.json());
-
 if (!mongoUri || !dbName) {
-  console.error('Missing MongoDB credentials');
+  console.error("Missing MongoDB credentials");
   process.exit(1);
 }
-
 const client = new MongoClient(mongoUri);
 
-client.connect().then(() => {
-  console.log('Connected to MongoDB');
-  const studentRepository = new MongoStudentRepository(client, dbName, 'students');
-  const studentController = new StudentController(studentRepository);
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to MongoDB");
+    const studentRepository = new MongoStudentRepository(
+      client,
+      dbName,
+      "students"
+    );
+    const studentController = new StudentController(studentRepository);
 
-  app.get('/students', (req, res) => studentController.getAll(req, res));
-  app.post('/students', (req, res) => studentController.add(req, res));
+    app.get("/students", (req, res) => studentController.getAll(req, res));
+    app.post("/students", (req, res) => studentController.add(req, res));
 
-  app.listen(port, () => {
-    console.log(`API running at ${port}`);
+    app.use(cors());
+    app.use(express.json());
+
+    app.listen(port, () => {
+      console.log(`API running at ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB", error);
   });
-}).catch(error => {
-  console.error('Failed to connect to MongoDB', error);
-});
 
 export { app };
