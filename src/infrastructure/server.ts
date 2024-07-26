89ff1444fa9248f2e5f7fdad20dee9021c1dbba4
David Ruiz <jesuruga@gmail.com>
2024-07-26 00:08:30 -0600

    [fix]

--- src/infrastructure/server.ts
+++ src/infrastructure/server.ts
@@ -5,18 +5,18 @@ import { MongoStudentRepository } from './repositories/MongoStudentRepository';
 import { StudentController } from './controllers/StudentController';
 import cors from 'cors';
 
-
-
-// Cargar variables de entorno
 dotenv.config();
 
-const app: Application = express(); 
-app.use(cors());
-app.use(express.json());
+const app: Application = express();
 const port = process.env.PORT || 3000;
 const mongoUri = process.env.MONGO_URI;
 const dbName = process.env.DB_NAME;
 
+// Configurar CORS para permitir todos los orígenes
+app.use(cors());
+
+// Configurar para parsear JSON
+app.use(express.json());
 
 if (!mongoUri || !dbName) {
   console.error('Missing MongoDB credentials');
@@ -29,8 +29,6 @@ client.connect().then(() => {
   console.log('Connected to MongoDB');
   const studentRepository = new MongoStudentRepository(client, dbName, 'students');
   const studentController = new StudentController(studentRepository);
-
-  app.use(express.json());
 
   app.get('/students', (req, res) => studentController.getAll(req, res));
   app.post('/students', (req, res) => studentController.add(req, res));
